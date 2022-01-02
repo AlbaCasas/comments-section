f769707807bebb6133bef5a525483fd5f1c6fda7
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-02 16:11:11 +0100

    Remove settings file

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import Layout from "./common/components/Layout";
 import "./common/style.scss";
 
 import Card from "./common/components/Card";
-import "./common/styles/_settings.scss";
 
 function App() {
   return (
