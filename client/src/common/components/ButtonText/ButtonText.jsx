f730cd461070e45fdf035b1d4edd26155d8a2854
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-02 19:49:06 +0100

    fix button text color not being applied to icon and text

--- client/src/common/components/ButtonText/ButtonText.jsx
+++ client/src/common/components/ButtonText/ButtonText.jsx
@@ -1,12 +1,13 @@
 import Text from "../Text";
 import Icon from "../Icon";
-import { getColorClassName } from "../../utils";
 
-const ButtonText = ({ color, children, iconName, className = "" }) => {
+const ButtonText = ({ color, children, iconName, className }) => {
   return (
-    <button className={`button-text ${getColorClassName(color)} ${className}`}>
-      <Icon name={iconName} className="button-text__icon" />
-      <Text isBold>{children}</Text>
+    <button className={`button-text ${className}`}>
+      <Icon name={iconName} className="button-text__icon" color={color} />
+      <Text isBold color={color}>
+        {children}
+      </Text>
     </button>
   );
 };
