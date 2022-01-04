3597f624efc61683dd6dd376ea6b36274cc900f7
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-04 11:07:16 +0100

    Add new comment on Send click

--- client/src/feed/components/AddComment/AddComment.jsx
+++ client/src/feed/components/AddComment/AddComment.jsx
@@ -2,12 +2,25 @@ import TextArea from "../../../common/components/TextArea";
 import Button from "../../../common/components/Button";
 import Avatar from "../../../common/components/Avatar";
 import Card from "../../../common/components/Card";
+import { getComments, createComment } from "../../services/comments";
+import { FeedContext } from "../../FeedContext";
+import { useContext } from "react";
 
 const AddComment = ({ userPicture, className = "" }) => {
+  const { setComments } = useContext(FeedContext);
+
   return (
     <Card className={`add-comment ${className}`}>
       <Avatar img={userPicture} className="add-comment__avatar" />
-      <form className="add-comment__form" onSubmit={(e) => {}}>
+      <form
+        className="add-comment__form"
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await createComment(e.target[0].value);
+          const comments = await getComments();
+          setComments(comments);
+        }}
+      >
         <TextArea placeholder="Add a comment..."></TextArea>
         <Button className="add-comment__button">send</Button>
       </form>
--- client/src/feed/services/comments.js
+++ client/src/feed/services/comments.js
@@ -9,3 +9,23 @@ export const deleteComment = async (id) => {
     method: "DELETE",
   });
 };
+
+export const createComment = async (content) => {
+  await fetch(`http://localhost:3001/comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      content: content,
+      createdAt: "1 second ago",
+      score: 0,
+      user: {
+        username: "apu",
+        image: {
+          png: "https://pbs.twimg.com/profile_images/755400180569141249/da18-Djn_400x400.jpg",
+        },
+      },
+    }),
+  });
+};
